f891f9672107ac6b4c78af42a4b941944e17750a
ghasemi <ghasemi@namatech.co.uk>
2023-02-26 01:27:01 +0330

    added loader comp

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -1,4 +1,19 @@
+import { loadavg } from "os";
+import { useContext } from "react";
+import { DataContext } from "../context/ContactsProvider";
+import Loader from "./Loader";
+
 const ContactsList = () => {
+  const { data, error, isLoading } = useContext(DataContext);
+
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return <div>ContactsList</div>;
 };
 
